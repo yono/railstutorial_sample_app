e2ff398d5eac0a72c7dc38a18a783970bf2f2de5
Yusaku ONO <yono.05@gmail.com>
2014-03-23 17:22:50 +0900

    fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,7 +57,7 @@ describe User do
                      foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
-        expect(@user).not_to be_valid
+        should_not be_valid
       end
     end
   end
@@ -67,7 +67,7 @@ describe User do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
-        expect(@user).to be_valid
+        should be_valid
       end
     end
   end
